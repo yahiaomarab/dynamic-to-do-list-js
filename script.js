c206c1778fcfd1bf39e067a02afc8ed79e72609f
yahiaomarab <yahiaomar746@gmail.com>
2025-01-26 22:51:03 +0200

    initiall commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText == "") {
+      alert("please Enter task");
+    }
+
+    item = document.createElement("li");
+    item.textContent = taskText;
+
+
+    removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.className = "remove-btn";
+    removeBtn.addEventListener("click", () => {
+      taskList.removeChild(item);
+    });
+
+
+    item.appendChild(removeBtn);
+
+    taskList.appendChild(item);
+
+    taskInput.value = "";
+  }
+
+  addButton.addEventListener("click", addTask);
+
+  taskInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
+  
+});
